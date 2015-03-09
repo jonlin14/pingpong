2baac5f31f0d716c13aa996a94c56dd3872f249d
Jonathan Lin & Conor Baumgart <student@epicodus.com>
2015-03-09 11:19:48 -0700

    Print 'Hello World'

--- views/front.php
+++ views/front.php
@@ -1,8 +1,13 @@
 <html>
     <head>
-        <title>Hello World!</title>
+        <title>Ping Pong</title>
     </head>
     <body>
-        <p>Hello World!</p>
+        <form action='jackson' method='post'>
+            <p><label for='limit1'>Enter a number</label></p>
+            <p><input id='limit1' name='limit1' type='number'></p>
+
+            <button type='submit'>Enter!</button>
+        </form>
     </body>
 </html>
